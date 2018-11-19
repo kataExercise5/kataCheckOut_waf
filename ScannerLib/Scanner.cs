1208437c11c652a69fb2d0b331e43874fd8c24eb
kataExercise5 <45145606+kataExercise5@users.noreply.github.com>
2018-11-19 15:15:25 -0500

    Add files via upload

--- ScannerLib/Scanner.cs
+++ ScannerLib/Scanner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ItemCatalogLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,108 @@ namespace ScannerLib
 {
     public class Scanner
     {
+        private ItemCatalog itemCatalog;
+        private Dictionary<int, decimal> checkOutSummary;
+        private string errorMsg;
+        public string getErrorMsg() { return errorMsg; }
+
+        public Scanner()
+        {
+            itemCatalog = new ItemCatalog();
+            checkOutSummary = new Dictionary<int, decimal>();
+        }
+
+        //ctor to support unit tests
+        public Scanner(Dictionary<int, decimal> testSum)
+        {
+            itemCatalog = new ItemCatalog();
+            checkOutSummary = testSum;
+        }
+
+        public bool initCheckOut(int checkOutNbr)
+        {
+            try
+            {
+                if (checkOutSummary.ContainsKey(checkOutNbr))
+                {
+                    checkOutSummary[checkOutNbr] = 0m;
+                }
+                else
+                {
+                    checkOutSummary.Add(checkOutNbr, 0m);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //use excp msg for now
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public decimal getCheckOutTotal(int checkOutNbr)
+        {
+            decimal checkOutTotal;
+            try
+            {
+                if (checkOutSummary.ContainsKey(checkOutNbr))
+                {
+                    checkOutTotal = checkOutSummary[checkOutNbr];
+                }
+                else
+                {
+                    checkOutTotal = 0m;
+                }
+                return checkOutTotal;
+            }
+            catch (Exception ex)
+            {
+                //exception handling tbd for now
+                throw ex;
+            }
+        }
+
+        public bool checkOutItem(int checkOutNbr, string itemName, int qty)
+        {
+            try
+            {
+                if (!checkOutSummary.ContainsKey(checkOutNbr))
+                {
+                    errorMsg = "This check out register has not been initialized.";
+                    return false;
+                }
+                decimal itemTotalPrice = itemCatalog.calcPrice(itemName, qty, new DateTime(2018, 11, 19));
+                checkOutSummary[checkOutNbr] = checkOutSummary[checkOutNbr] + itemTotalPrice;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //use excp msg for now
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
+
+        public bool checkOutItem(int checkOutNbr, string itemName, decimal pounds)
+        {
+            try
+            {
+                if (!checkOutSummary.ContainsKey(checkOutNbr))
+                {
+                    errorMsg = "This check out register has not been initialized.";
+                    return false;
+                }
+                decimal itemTotalPrice = itemCatalog.calcPrice(itemName, pounds, new DateTime(2018, 11, 19));
+                checkOutSummary[checkOutNbr] = checkOutSummary[checkOutNbr] + itemTotalPrice;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //use excp msg for now
+                errorMsg = ex.Message;
+                return false;
+            }
+        }
     }
 }
---
+++ ScannerLibTests/ScannerTests.cs
@@ -0,0 +1,68 @@
+﻿using ItemCatalogLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScannerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerLib.Tests
+{
+    [TestClass()]
+    public class ScannerTests
+    {
+        private Dictionary<int, decimal> checkOutSummary;
+        private Scanner scanner;
+
+        //attributes for ItemCatalog
+        private ItemCatalog itemCatalog;
+        private string applesItemName = "apples";
+        private string tideItemName = "tide";
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            checkOutSummary = new Dictionary<int, decimal>();
+            checkOutSummary.Add(3, 0m);
+            scanner = new Scanner(checkOutSummary);
+
+            //init for ItemCatalog
+            Dictionary<string, Item> catalog = new Dictionary<string, Item>();
+
+            Item item = new Item(applesItemName, .50m, Item.Units.pounds);
+            catalog.Add(applesItemName, item);
+
+            item = new Item(tideItemName, 4.77m, Item.Units.each);
+            catalog.Add(tideItemName, item);
+
+            itemCatalog = new ItemCatalog(catalog);
+        }
+
+        [TestMethod()]
+        public void initCheckOutTestNewNbr()
+        {
+            Assert.IsTrue(scanner.initCheckOut(4));
+        }
+
+        [TestMethod()]
+        public void initCheckOutTestExistingNbr()
+        {
+            Assert.IsTrue(scanner.initCheckOut(3));
+        }
+
+        [TestMethod()]
+        public void checkOutItemEachExists()
+        {
+            scanner.checkOutItem(3, tideItemName, 2);
+            Assert.IsTrue(scanner.getCheckOutTotal(3) == 9.54m);
+        }
+
+        [TestMethod()]
+        public void checkOutItemPoundsExists()
+        {
+            scanner.checkOutItem(3, applesItemName, 8.5m);
+            Assert.IsTrue(scanner.getCheckOutTotal(3) == 4.25m);
+        }
+    }
+}
